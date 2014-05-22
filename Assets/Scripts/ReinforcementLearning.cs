433d23d5526f03697a740cac2d56a3a3694fe926
FlorianBasso <florian@ideanov.com>
2014-05-22 17:46:38 +0200

    change Q, states, actions array

--- Assets/Scripts/ReinforcementLearning.cs
+++ Assets/Scripts/ReinforcementLearning.cs
@@ -9,16 +9,16 @@ public class ReinforcementLearning : MonoBehaviour {
 	public float alpha = 0.2;
 	public float gamma = 0.9;
 
-	private string[] actions;
-	private string[] states;
-	private string[] q;
+	private int[] actions = new int[4];
+	private int[] states = new int[3];
+	private int[] q;
 	// Use this for initialization
 	void Start () {
 	
 		//Initialize array
-		actions = new string["push", "turn", "growFlower", "growLeaf"];
-		states = new string["rootPosition", "flowerPosition", "leafPosition"];
-		q = new string[actions] [states];
+//		actions = new string["push", "turn", "growFlower", "growLeaf"];
+//		states = new string["rootPosition", "flowerPosition", "leafPosition"];
+		q = new int[actions.Length] [states.Length];
 	}
 	
 	// Update is called once per frame
